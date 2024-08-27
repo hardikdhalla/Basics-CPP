1dccd3b0040078621adb3f746cea9dd93fc37245
hardikdhalla <dhallahardik31@gmail.com>
2024-08-27 12:56:28 +0530

    Create helloworld.cpp

---
+++ helloworld.cpp
@@ -0,0 +1,13 @@
+//Name - Hardik Dhalla
+//PRN - 23070123058
+//Aim - Printing out Hello World
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World";
+    return 0;
+}
+/* Output:
+Hello World
+*/
